7371d61baa9853dd7b495dac00b619a9bd96540b
zakzheng <zyqlzr@gmail.com>
2018-01-30 15:29:29 +0800

    add sl_agent vs built-in

---
+++ battle/bin/slagent_vs_builtin.py
@@ -0,0 +1,21 @@
+from battle.arena_sc2 import SC2WrapperAgent, make, ids
+
+import battle.arena_sc2.arenas
+import battle.arena_sc2.agents
+import battle.arena_sc2.envs
+
+from absl import flags
+from absl import app
+
+def run_agent_vs_builtin(unused_argv):
+  print("all register id=", ids())
+  sc2_battle = make('AgentVSBuiltIn')
+  sc2_battle.register_agent(
+      SC2WrapperAgent(make('SLAgent', model_path='/home/zakzheng/job/git/battle_ai/battle/battle/model/agent.model-2540000')))
+  sc2_battle.register_env(make('sc2env'))
+  sc2_battle.play_game()
+  print(sc2_battle.stat)
+
+if __name__ == '__main__':
+  app.run(run_agent_vs_builtin)
+
